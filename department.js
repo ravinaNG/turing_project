50902195edbfbc966c82289e66a18e3c7aa11e32
ravinaNG <ravina18@navgurukul.org>
2019-12-30 00:23:07 +0530

    getting objects by using id, it's second task:) done

--- department.js
+++ department.js
@@ -3,12 +3,23 @@ const app = express();
 const knex = require('./knexFile');
 
 app.get('/departments', (req,res) => {
-    knex.select("*").from("department").then((data) => {
+    knex.select('*').from("department").then((data) => {
         res.send(data)
     })
 })
 
+// by id
+
+app.get('/departments/:id', (req,res) => {
+    // let department_id = req.params.id
+    knex.select("*")
+    .from('department')
+    .where('department.department_id', req.params.id)
+    .then((data) => {
+        res.send(data)
+    })
+});
+
 app.listen(7000, () => {
     console.log('server is running....')
 });
-
