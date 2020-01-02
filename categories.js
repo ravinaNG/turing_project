2ad4f768e0c0b4ddfeb811984ac3d4612e7b5d9b
ravinaNG <ravina18@navgurukul.org>
2020-01-02 11:46:19 +0530

    fourth task done I have used here join method to join tables:)

--- categories.js
+++ categories.js
@@ -11,17 +11,27 @@ app.get('/category', (req,res) => {
 })
 
 // by id
-
-app.get('/category/:id'), (req, res) =>{
-    // const id = req.params.id
+app.get('/category/:id', (req,res) => {
+    const id = req.params.id;
     knex.select('*')
-    .from('category')
-    .where('category.category_id', id)
+    .from("category")
+    .where('category.category_id',id)
     .then((data) => {
         res.send(data)
     })
-}
+})
 
-app.listen(3000, () => {
+app.get('/categories/inProduct/:id', (req, res) => {
+    const id = req.params.id;
+    knex('category')
+    .select('category.category_id', 'department_id', 'name')
+    .join('product_category', 'category.category_id', '=', 'product_category.category_id')
+    .where('product_category.product_id', id)
+    .then((data) => {
+        res.send(data)
+    })
+})
+
+app.listen(7000, () => {
     console.log('Server is active....')
 })
