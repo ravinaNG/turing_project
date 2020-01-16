ddd5ed38826d893be5236ed6329e0c58ff7f52d4
ravinaNG <ravina18@navgurukul.org>
2020-01-17 00:54:42 +0530

    -=-  orders one done, Turing Project only done  -=-

--- models/ordersDB.js
+++ models/ordersDB.js
@@ -10,4 +10,26 @@ const orders = (data) => {
     return knex('orders').insert(data);
 };
 
-module.exports = {orders, joinedData}
+const order_id = (id) => {
+    return knex('orders').select('order_id').where('orders.order_id', id)
+}
+
+const dataFromProduct = (id) => {
+    return knex('product')
+    .join('saveForLater', 'saveForLater.product_id', '=', 'product.product_id')
+    .select('product.product_id', 'product.name', 'product.price', 'saveForLater.quantity', 'saveForLater.attributes', 'saveForLater.item_id')
+    .where('product.product_id', id)
+}
+
+const insertingData = (data) => {
+    return knex('order_detail').insert(data);
+}
+
+const inCustomer = () => {
+    return knex.select('*').from('orders');
+}
+
+const shortDetails = (id) => {
+    return knex.select('*').from('orders').where('orders.order_id', id);
+}
+module.exports = {orders, joinedData, order_id, dataFromProduct, insertingData, insertingData, inCustomer, shortDetails}
--- routes/orders.js
+++ routes/orders.js
@@ -8,24 +8,105 @@ router.post('/order', (req, res) => {
     const tokenList = allTokens.split('=undefined; ')
     const lastToken = tokenList[tokenList.length - 1];
     const priceNquantity = ordersDB.joinedData();
-    // console.log(priceNquantity);
-    // jwt.verify(lastToken, 'Ravina', () => {
-    //     const dataToPost = {
-    //         "order_id": req.body.order_id,
-    //         "total_amount": priceNquantity[0]['quantity'] * priceNquantity[0]['price'],
-    //         "created_on": Date,
-    //         "shipped_on": Date,
-    //         "status": req.body.status,
-    //         "comments": req.body.comments,
-    //         "customer_id": req.body.customer_id,
-    //         "auth_code": req.body.auth_code,
-    //         "reference": req.body.reference,
-    //         "shipping_id": req.body.shipping_id,
-    //         "tax_id": req.body.tax_id 
-    //     }
-    //     const postedData = ordersDB.orders(dataToPost);
-        priceNquantity.then((resp) => {
-            console.log(resp);
+    priceNquantity.then((priceNquantity) => {
+        jwt.verify(lastToken, 'Ravina', () => {
+            const total_amount = priceNquantity[0]['quantity'] * priceNquantity[0]['price']
+            const dataToPost = {
+                "order_id": req.body.order_id,
+                "total_amount": total_amount,
+                "created_on": new Date,
+                "shipped_on": new Date,
+                "status": req.body.status,
+                "comments": req.body.comments,
+                "customer_id": req.body.customer_id,
+                "auth_code": req.body.auth_code,
+                "reference": req.body.reference,
+                "shipping_id": req.body.shipping_id,
+                "tax_id": req.body.tax_id
+            }
+            const postedData = ordersDB.orders(dataToPost);
+            postedData.then((resp) => {
+                console.log('Done bhai maze kar ab ....')
+                res.json(resp);
+            })
+            .catch((err) =>{
+                res.send(err);
+            })
+        console.log(resp);
+        res.json(resp);
+        })
+        .catch((err) => {
+            res.send(err);
+        })
+    })
+});
+
+router.get('/order_id/:id', (req, res) => {
+    const allTokens = req.headers.cookie;
+    const tokenList = allTokens.split('=undefined; ')
+    const firstToken = tokenList[0];
+    const order_id = req.params.id;
+    const conformed_id = ordersDB.order_id(order_id);
+    const dataToPost = ordersDB.dataFromProduct(conformed_id);
+    dataToPost.then((resp) => {
+        jwt.verify(firstToken, 'Ravina', () => {
+            const id = conformed_id;
+            const product_id = resp[0]['product_id'];
+            const attributes = resp[0]['attributes'];
+            const product_name = resp[0]['name'];
+            const quantity = resp[0]['quantity'];
+            const unit_cost = quantity * resp[0]['price'];
+            const item_id = resp[0]['item_id'];
+
+            const dataToInsert = {
+                "item_id": item_id,
+                "order_id": id,
+                "product_id": product_id,
+                "attributes": attributes,
+                "product_name": product_name,
+                "quantity": quantity,
+                "unit_cost": unit_cost,
+            }
+
+            const postedData = ordersDB.insertingData(dataToInsert);
+            postedData.then((response) => {
+                console.log("Injoy yourself");
+                res.json(response);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.send(err);
+            })
+        })
+    })
+})
+
+router.get('/inCustomer', (req, res) => {
+    const allTokens = req.headers.cookie;
+    const tokenList = allTokens.split('=undefined; ')
+    const secondToken = tokenList[1];
+
+    jwt.verify(secondToken, 'Ravina', () => {
+        const data = ordersDB.inCustomer();
+        data.then((resp) => {
+            res.json(resp);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        })
+    })
+})
+
+router.get('/shortDetail/:id', (req, res) => {
+    const order_id = req.params.id;
+    const allTokens = req.headers.cookie;
+    const tokenList = allTokens.split('=undefined; ')
+    const secondToken = tokenList[1];
+
+    jwt.verify(secondToken, 'Ravina', () => {
+        const idWiseData = ordersDB.shortDetails(order_id);
+        idWiseData.then((resp) => {
             res.json(resp);
         })
         .catch((err) => {
